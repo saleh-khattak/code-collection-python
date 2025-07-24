055d53ce6cd900373eed88b64a5bd06435c4ab94
saleh-khattak <muhammadsaleh79777@gmail.com>
2025-07-24 23:14:53 +0500

    Chep 9:File input and output

---
+++ search_python.py
@@ -0,0 +1,6 @@
+with open("pythonfile.txt","r") as f:
+    content=f.read()
+if ("python" in content):
+    print("The word python is present in file")
+else:
+    print("The word python is present in file")
