5ecfa272aa24c7422556a964b70c4c55c9c68b3d
saleh-khattak <muhammadsaleh79777@gmail.com>
2025-07-25 22:22:53 +0500

    Chep 10:OOP

---
+++ calculator_square_squareRoot_cube.py
@@ -0,0 +1,14 @@
+class Calculator:
+    def __init__(self,n):
+        self.n=n
+    def square(self):
+        print(f"The square is {self.n*self.n}")
+    def cube(self):
+        print(f"The cube is {self.n*self.n*self.n}")
+    def squareRoot(self):
+        print(f"The squareroot is {self.n**1/2}")
+
+calculator=Calculator(4)
+calculator.square()
+calculator.cube()
+calculator.squareRoot()
